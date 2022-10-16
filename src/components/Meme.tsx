80fad10930a2e0b0ba27f3c3dc4d86599bce687f
tim <tfierek357@gmail.com>
2022-10-16 15:22:36 -0400

    styling and onclick added

--- src/components/Meme.tsx
+++ src/components/Meme.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import memesData from "../memesData";
 
 function Meme() {
+    function getRandomMeme(){
+        const memes = memesData.data.memes;
+        const meme = memes[Math.floor(Math.random()*memes.length)];
+        
+    }
+
     return(
         <div className="meme">
-            <form action="">
+            <div className="memeForm">
                 <div className="inputs">
                     <input type="text" placeholder="top text"/>
                     <input type="text" placeholder="bottom text"/>
                 </div>
-                <button>Get a new meme image</button>
-            </form>
+                <button onClick={getRandomMeme}>Get a new meme image</button>
+            </div>
         </div>
     );
 }
